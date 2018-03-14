91d2c50e60ea0f297c84a1df8700bcbe9e6abe93
Tarnation93 <vukasinkreten@yahoo.com>
2018-03-14 21:29:26 +0100

    adding database connection

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '1',
-    database : 'smart-brain'
+connectionString: process.env.DATABASE_URL,
+  ssl: true,
   }
 });
 db.select('*').from ('users').then(data=>{
